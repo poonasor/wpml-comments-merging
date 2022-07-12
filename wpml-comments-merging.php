d7008265648ee7d9dcc8380c6521a0df2c9f4739
Ricky Poon <poonasor@users.noreply.github.com>
2022-07-12 14:11:13 -0400

    Update wpml-comments-merging.php
    Added $sitepress class, removed comments_clauses filter

--- wpml-comments-merging.php
+++ wpml-comments-merging.php
@@ -1,34 +1,34 @@
 <?php
 /*
-Plugin Name: WPML comments merging
-Plugin URI: https://github.com/jgalea/wpml-comments-merging
-Description: This plugin merges comments from all translations of the posts and pages, so that they all are displayed on each other. Comments are internally still attached to the post or page they were made on.
+Plugin Name: WPML Merged Comments
+Plugin URI: https://rickypoon.ca/
+Description: WPML: Show Reviews/Comments in All Languages
 Version: 2.0
-Author: Jean Galea
-Author URI: http://www.jeangalea.com
+Author: Ricky Poon
+Author URI: https://rickypoon.ca/
 License: GPL
 
 This is a fixed version of the no longer maintained WPML Comment Merging plugin:
 http://wordpress.org/extend/plugins/wpml-comment-merging/
 Thanks to Simon Wheatley for contributing the fix.
-*/
 
+Forked from: https://github.com/jgalea/wpml-comments-merging
+*/
+global $sitepress;
 function sort_merged_comments($a, $b) { 
 	return $a->comment_ID - $b->comment_ID;
 }
-
 function merge_comments($comments, $post_ID) {
-	global $sitepress;
-	remove_filter( 'comments_clauses', array( $sitepress, 'comments_clauses' ) );
 	// get all the languages for which this post exists
 	$languages = icl_get_languages('skip_missing=1');
 	$post = get_post( $post_ID );
 	$type = $post->post_type;
-	foreach($languages as $code => $l) {
+	foreach($languages as $l) {
 		// in $comments are already the comments from the current language
 		if(!$l['active']) {
 			$otherID = icl_object_id($post_ID, $type, false, $l['language_code']);
-			$othercomments = get_comments( array('post_id' => $otherID, 'status' => 'approve', 'order' => 'ASC') );
+			//$othercomments = get_comments($otherID);
+			$othercomments = get_approved_comments($otherID);
 			$comments = array_merge($comments, $othercomments);
 		}
 	}
@@ -36,9 +36,6 @@ function merge_comments($comments, $post_ID) {
 		// if we merged some comments in we need to reestablish an order
 		usort($comments, 'sort_merged_comments');
 	}
-	//
-	add_filter( 'comments_clauses', array( $sitepress, 'comments_clauses' ) );
-
 	return $comments;
 }
 function merge_comment_count($count, $post_ID) {
@@ -46,7 +43,6 @@ function merge_comment_count($count, $post_ID) {
 	$languages = icl_get_languages('skip_missing=1');
 	$post = get_post( $post_ID );
 	$type = $post->post_type;
-
 	foreach($languages as $l) {
 		// in $count is already the count from the current language
 		if(!$l['active']) {
@@ -67,6 +63,6 @@ function merge_comment_count($count, $post_ID) {
 	}
 	return $count;
 }
-
+remove_filter('comments_clauses', array( $sitepress, 'comments_clauses' ), 10, 2);
 add_filter('comments_array', 'merge_comments', 100, 2);
 add_filter('get_comments_number', 'merge_comment_count', 100, 2);
